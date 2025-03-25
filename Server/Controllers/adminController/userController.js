533f9e7ac542139921adfbda757ed61e92cedf59
fida-ghourabi <fidagh803@gmail.com>
2025-03-25 15:59:52 +0100

    ValidatedUsers

--- Server/Controllers/adminController/userController.js
+++ Server/Controllers/adminController/userController.js
@@ -4,32 +4,35 @@ const SubscriptionType = require("../../Models/subscriptionType");
 
 // Get all validated users
 const getValidatedUsers = async (req, res) => {
-    try {
-      const users = await User.find({ isValidated: true })
-        .select('prenom nom email phone')
-        .populate({
-          path: 'subscription',
-          populate: {
-            path: 'type',
-            model: 'SubscriptionType',
-            select: 'name'
-          },
-          select: 'status'
-        });
-  
-      const formattedUsers = users.map(user => ({
+  try {
+    // Récupérer tous les adhérents validés
+    const users = await User.find({ isValidated: true, role: "adherent" })
+      .select("prenom nom email phone");
+
+    // Récupérer les abonnements pour ces utilisateurs
+    const subscriptions = await Subscription.find({
+      user: { $in: users.map((u) => u._id) }
+    })
+      .populate({ path: "type", select: "name" }) // Récupérer le type d'abonnement
+      .select("user status");
+
+    // Associer chaque utilisateur avec son abonnement
+    const formattedUsers = users.map(user => {
+      const userSubscription = subscriptions.find(sub => sub.user.toString() === user._id.toString());
+      return {
         prenom: user.prenom,
         nom: user.nom,
         email: user.email,
-        email: user.phone,
-        abonnementType: user.subscription?.type?.name || null,
-        abonnementStatus: user.subscription?.status || null
-      }));
-  
-      res.status(200).json({ users: formattedUsers });
-    } catch (error) {
-      res.status(500).json({ msg: 'Server Error', error });
-    }
+        phone: user.phone,
+        abonnementType: userSubscription?.type?.name || null,
+        abonnementStatus: userSubscription?.status || null
+      };
+    });
+
+    res.status(200).json({ users: formattedUsers });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error", error });
+  }
   };
 
 // Get all non-validated users
