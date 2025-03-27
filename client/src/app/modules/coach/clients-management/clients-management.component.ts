68e99bdbbbc234ac7716f60a7f346c4d28a4436b
yassine-mathlouthi <yassinemt08@gmail.com>
2025-03-27 03:55:21 +0100

    coach layout

---
+++ client/src/app/modules/coach/clients-management/clients-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './clients-management.component.html',
+  styleUrl: './clients-management.component.css'
+})
+export class ClientsManagementComponent {
+
+}
