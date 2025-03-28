2a4df083d822deaa381b2455f34a3773f7b47ac6
yassine-mathlouthi <yassinemt08@gmail.com>
2025-03-28 04:28:38 +0100

    add dynamique home page , enhace style

--- client/src/app/modules/home/home.component.ts
+++ client/src/app/modules/home/home.component.ts
@@ -1,13 +1,30 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../../core/services/courses.service';
+import { SubscriptionsService } from '../../core/services/subscriptions.service';
 import { NavbarComponent } from '../../main-layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent  {
+export class HomeComponent implements OnInit {
+  constructor(private _coursesService:CoursesService,private _subscriptionService:SubscriptionsService){}
+  data:any
+  Tarif:any
+  ngOnInit(): void {
+    this._coursesService.getAllCourses().subscribe(r=>{
+      this.data=r
+      console.log(r)
+    })
+    this._subscriptionService.getSubscriptionTypes().subscribe(r=>{
+      console.log(r)
+      this.Tarif=r;
+    })
+
+  }
   
 }
